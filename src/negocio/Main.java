0e0fbbac409be552178427252daa485f86600836
Bianca Gotaski <biancagotaski@outlook.com.br>
2019-06-18 12:55:34 -0300

    v1 finished

--- src/negocio/Main.java
+++ src/negocio/Main.java
@@ -1,37 +1,46 @@
 package negocio;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	static Scanner scanner = new Scanner(System.in);
-	static String input;
 	static DBConnection db = new DBConnection();
+	static String input;
+	static String cidade;
+	static int pais;
+	static String stopApplication = "3";
 	
 	public static void main(String[] args) {
 		menu();
-		String optionCidade = cadastrarCidade();
-		int optionPais = escolherPais();
-		listarCidades();
-		cadastrar(optionCidade, optionPais);
 	}
 	
-	public static String menu() {
-		System.out.println("1. Cadastrar cidade \n"
-				+ "2. Listar cidades"
-				);
-		
-		input = scanner.nextLine();
+	public static void menu() {
+		do {
+			System.out.println("1. Cadastrar cidade \n"
+					+ "2. Listar cidades\n"
+					);
 			
-		return input;
-	}
-	
-	public static String cadastrarCidade() {
-		System.out.println("Escreva o nome da cidade: \n");
-		String cidade = scanner.nextLine();
-		
-		return cidade;
+			input = scanner.nextLine();
+				
+			switch(input) {
+				case "1":
+					System.out.println("Escreva o nome da cidade: \n");
+					cidade = scanner.nextLine();
+					pais = escolherPais();
+					cadastrar(cidade, pais);
+					break;
+				case "2":
+					listarCidades();
+					break;
+				case "3":
+					input = stopApplication;
+					break;
+			}
+			System.out.println();
+		} while(input != stopApplication);
 	}
 	
 	public static int escolherPais() {
@@ -67,8 +76,6 @@ public class Main {
 			db.listarCidades();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		
+		}	
 	}
-	
 }
